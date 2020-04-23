d151109c034ac254dff06058e7b046eda1a045d6
Avishkar Bhoopchand <avishkar@google.com>
2020-04-23 15:48:46 +0100

    Added updated owid datasets to init

--- covid19_datasets/__init__.py
+++ covid19_datasets/__init__.py
@@ -2,4 +2,4 @@ from .oxford_government_policy import OxfordGovernmentPolicyDataset
 from .world_bank import WorldBankDataBank, WORLD_BANK_INDICATORS, AGGREGATES
 from .yougov_behavioural_tracker import YouGovBehaviouralTracker
 from .acaps_government_measures import AcapsGovernmentMeasures
-from .our_world_in_data import OurWorldInData
+from .our_world_in_data import OWIDCovid19, OWIDMedianAges
