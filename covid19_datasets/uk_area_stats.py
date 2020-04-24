6d3e54667e297fdb4525692826edb0a15a0fa709
Andrei Paleyes <apalees@gmail.com>
2020-04-24 17:25:30 +0100

    Deprecate UK deaths dataset for now

--- covid19_datasets/uk_area_stats.py
+++ covid19_datasets/uk_area_stats.py
@@ -67,31 +67,32 @@ class UKCovid19Cases:
         return UKCovid19Cases.data
 
 
-class UKCovid19Deaths:
-    """
-    New COVID-19 deaths in each UK area
+# Deprecated for now - csv appreas to be in a different format
+# class UKCovid19Deaths:
+#     """
+#     New COVID-19 deaths in each UK area
     
-    Format is:
-    - Each row corresponds to an area
-    - Each column corresponds to a date
-    - Each cell contains daily deaths count
-    """
+#     Format is:
+#     - Each row corresponds to an area
+#     - Each column corresponds to a date
+#     - Each cell contains daily deaths count
+#     """
 
-    data = None
+#     data = None
 
-    def __init__(self, force_load=False):
-        """
-        Loads the dataset and stores it in memory.
-        Further instances of this class will reuse the same data
+#     def __init__(self, force_load=False):
+#         """
+#         Loads the dataset and stores it in memory.
+#         Further instances of this class will reuse the same data
 
-        :param force_load: If true, forces download of the dataset, even if it was loaded already
-        """
-        # This is to make sure we only load the dataset once during a single session
-        if UKCovid19Deaths.data is None or force_load:
-            UKCovid19Deaths.data = _load_dataset(ENGLAND_DEATHS_PATH)
+#         :param force_load: If true, forces download of the dataset, even if it was loaded already
+#         """
+#         # This is to make sure we only load the dataset once during a single session
+#         if UKCovid19Deaths.data is None or force_load:
+#             UKCovid19Deaths.data = _load_dataset(ENGLAND_DEATHS_PATH)
 
-    def get_data(self):
-        """
-        Returns the dataset as Pandas dataframe
-        """
-        return UKCovid19Deaths.data
+#     def get_data(self):
+#         """
+#         Returns the dataset as Pandas dataframe
+#         """
+#         return UKCovid19Deaths.data
