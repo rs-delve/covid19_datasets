929e22513e7788ef8f65f883c9bd886938b4866e
Andrei Paleyes <apalees@gmail.com>
2020-04-29 09:48:05 +0100

    Added Scotland

--- covid19_datasets/uk_area_stats.py
+++ covid19_datasets/uk_area_stats.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import datetime
 from .constants import DATE_COLUMN_NAME
 
 import logging
@@ -11,6 +12,7 @@ ENGLAND_DEATHS_PATH = 'https://c19downloads.azureedge.net/downloads/csv/coronavi
 
 WALES_PATH = 'http://www2.nphs.wales.nhs.uk:8080/CommunitySurveillanceDocs.nsf/61c1e930f9121fd080256f2a004937ed/77fdb9a33544aee88025855100300cab/$FILE/Rapid%20COVID-19%20surveillance%20data.xlsx'
 
+SCOTLAND_PATH = 'https://raw.githubusercontent.com/DataScienceScotland/COVID-19-Management-Information/master/COVID19%20-%20Daily%20Management%20Information%20-%20Scottish%20Health%20Boards%20-%20Cumulative%20cases.csv'
 
 def _backfill_missing_data(df):
     """
@@ -76,6 +78,33 @@ def _load_wales_datasets():
     return df_cases, df_tests
 
 
+def _load_scotland_cases_dataset():
+    _log.info("Loading dataset from " + SCOTLAND_PATH)
+    df = pd.read_csv(SCOTLAND_PATH)
+    _log.info("Loaded")
+
+    # downloaded file is (dates x areas), and we want the opposite
+    df = df.transpose()
+
+    # turn first row into a header
+    new_header = df.iloc[0]
+    df = df[1:]
+    df.columns = pd.to_datetime(new_header.astype(str))
+    df.columns.name = None
+
+    df = df.replace('*', 0.0).astype(float)
+
+    # original has cumulative data, and we want new cases per day
+    for i in range(len(df.columns) - 1, 1, -1):
+        df.iloc[:, i] = df.iloc[:, i] - df.iloc[:, i-1]
+
+    # set multi index by country and area
+    df['Country'] = 'Scotland'
+    df = df.reset_index().rename(columns={'index': 'Area name'}).set_index(['Country', 'Area name'])
+
+    return df
+
+
 class UKCovid19Data:
     """
     Provides COVID-19 data for various parts of the UK
@@ -84,6 +113,7 @@ class UKCovid19Data:
     england_cases_data = None
     wales_cases_data = None
     wales_tests_data = None
+    scotland_cases_data = None
 
     def __init__(self, force_load=False):
         """
@@ -98,16 +128,21 @@ class UKCovid19Data:
         if UKCovid19Data.wales_cases_data is None or UKCovid19Data.wales_tests_data is None or force_load:
             UKCovid19Data.wales_cases_data, UKCovid19Data.wales_tests_data = _load_wales_datasets()
 
+        if UKCovid19Data.scotland_cases_data is None or force_load:
+            UKCovid19Data.scotland_cases_data = _load_scotland_cases_dataset()
+
     def get_cases_data(self):
         """
         Returns the dataset as Pandas dataframe
-        
+
         Format:
-        - Row index: Country (England and Wales so far), County
+        - Row index: Country (England, Wales or Scotland), Area name
         - Columns: Dates
         - Each cell value is a number of new cases registered on that day
+
+        Note: Scotland provides data by NHS Board, not by county
         """
-        df = pd.concat([UKCovid19Data.england_cases_data, UKCovid19Data.wales_cases_data])
+        df = pd.concat([UKCovid19Data.england_cases_data, UKCovid19Data.wales_cases_data, UKCovid19Data.scotland_cases_data])
         # in case they have uneven number of columns
         df = df.fillna(0.0)
 
