af8956ad92987a7887e256560b9632ea5448387c
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-05-19 18:18:03 +0100

    Added Eurostats to combined data

--- covid19_datasets/combined.py
+++ covid19_datasets/combined.py
@@ -9,6 +9,7 @@ from .mask_policies import MaskPolicies
 from .world_bank import WorldBankDataBank
 from .mobility import Mobility
 from .economist_excess_mortality import EconomistExcessMortality
+from .eurostats import EuroStatsExcessMortality
 from .constants import ISO_COLUMN_NAME, DATE_COLUMN_NAME
 
 
@@ -17,6 +18,8 @@ _OWID_COVID19_DROP_COLUMNS = ['tests_units', 'location']
 _OWID_AGE_DROP_COLUMNS = ['Entity', 'Year']
 _MASKS_DROP_COLUMNS = ['Country', 'Source']
 _WORLD_BANK_DROP_COLUMNS = ['country', 'Smoking prevalence, females (% of adults)', 'Smoking prevalence, males (% of adults)', 'Diabetes (% of population ages 20 to 79)']
+_EUROSTATS_EXCLUDE = ['ESP', 'PRT', 'SWE']
+_ECONOMIST_EXCLUDE = ['AUT', 'BEL', 'CHE', 'DNK', 'NOR']
 
 
 def _policies_data() -> pd.DataFrame:
@@ -50,9 +53,30 @@ def _mobility_data() -> pd.DataFrame:
 
 
 def _excess_mortality_data() -> pd.DataFrame:
-    excess_mortality = EconomistExcessMortality()
-    data = excess_mortality.get_country_level_data(daily=True)
-    return data[[ISO_COLUMN_NAME, DATE_COLUMN_NAME, 'excess_death_daily_avg', 'weekly_excess_deaths']]
+    economist_excess_mortality = EconomistExcessMortality()
+    economist_data = economist_excess_mortality.get_country_level_data(daily=True)
+    economist_data = economist_data[~economist_data[ISO_COLUMN_NAME].isin(_ECONOMIST_EXCLUDE)]
+
+    eurostats_mortality = EuroStatsExcessMortality()
+    eurostats_data = eurostats_mortality.get_data(daily=True)
+    eurostats_data = eurostats_data.query('SEX == "Total" and AGE == "Total"')
+    eurostats_data = eurostats_data[~eurostats_data[ISO_COLUMN_NAME].isin(_EUROSTATS_EXCLUDE)]
+    eurostats_data = eurostats_data.rename(columns={
+        'excess_mortality': 'excess_death',
+        'excess_mortality_daily_average': 'excess_death_daily_avg'
+        })
+
+    columns = [ISO_COLUMN_NAME, DATE_COLUMN_NAME, 'excess_death_daily_avg', 'weekly_excess_deaths']
+
+    intersection = set(eurostats_data[ISO_COLUMN_NAME]) & set(economist_data[ISO_COLUMN_NAME])
+    if intersection:
+        raise ValueError('Found duplicated ISOs in Economist and EuroStats excess mortality data: ' + intersection)
+
+    excess_mortality = pd.concat([
+        economist_data[columns],
+        eurostats_data[columns]
+    ], axis=0) 
+    return excess_mortality
 
 
 def _create_interventions_data() -> pd.DataFrame:
