8e74c6b6dac7fbf60636273339d25990fb4ba0c7
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-05-26 16:00:13 +0100

    Added HMD to excess mortality

--- covid19_datasets/excess_mortality.py
+++ covid19_datasets/excess_mortality.py
@@ -8,43 +8,49 @@ import logging
 
 from .economist_excess_mortality import EconomistExcessMortality
 from .eurostats import EuroStatsExcessMortality
+from .hmd import HMDExcessMortality
 
 from .constants import *
 from .utils import get_country_iso
 
 _log = logging.getLogger(__name__)
 
-_EUROSTATS_EXCLUDE = ['ESP', 'PRT', 'SWE']
-_ECONOMIST_EXCLUDE = ['AUT', 'BEL', 'CHE', 'DNK', 'NOR']
+_HMD_COUNTRIES = ['AUT', 'BEL', 'DNK', 'FIN', 'ESP', 'ISL', 'NLD', 'NOR', 'PRT', 'SWE', 'USA']
+_EUROSTATS_COUNTRIES = ['ARM', 'BGR', 'CZE', 'EST', 'GEO', 'LVA', 'LIE', 'LTU', 'LUX', 'MNE', 'SRB', 'SVK', 'SVN', 'CHE']
+_ECONOMIST_COUNTRIES = [
+    'GBR',  # Economist dataset combines England, Wales, Scotland and Northern Ireland unlike HMD and EuroStats
+    'ECU',
+    'FRA',
+    'DEU',
+    'ITA'
+]
+
+assert len(set(_HMD_COUNTRIES + _EUROSTATS_COUNTRIES + _ECONOMIST_COUNTRIES)) == len(_HMD_COUNTRIES + _EUROSTATS_COUNTRIES + _ECONOMIST_COUNTRIES)
 
 
 def _generate_excess_mortality() -> pd.DataFrame:
     economist_excess_mortality = EconomistExcessMortality()
-    economist_data = economist_excess_mortality.get_country_level_data(
-        daily=True)
-    economist_data = economist_data[~economist_data[ISO_COLUMN_NAME].isin(
-        _ECONOMIST_EXCLUDE)]
+    economist_data = economist_excess_mortality.get_country_level_data(daily=True)
+    economist_data = economist_data[economist_data[ISO_COLUMN_NAME].isin(_ECONOMIST_COUNTRIES)]
 
     eurostats_mortality = EuroStatsExcessMortality()
     eurostats_data = eurostats_mortality.get_data(daily=True)
     eurostats_data = eurostats_data.query('SEX == "Total" and AGE == "Total"')
-    eurostats_data = eurostats_data[~eurostats_data[ISO_COLUMN_NAME].isin(
-        _EUROSTATS_EXCLUDE)]
+    eurostats_data = eurostats_data[eurostats_data[ISO_COLUMN_NAME].isin(_EUROSTATS_COUNTRIES)]
 
-    columns = [ISO_COLUMN_NAME, DATE_COLUMN_NAME,
-               'deaths_excess_daily_avg', 'deaths_excess_weekly']
+    hmd_mortality = HMDExcessMortality()
+    hmd_data = hmd_mortality.get_data(daily=True)
+    hmd_data = hmd_data.query('Sex == "Total"')
 
-    intersection = set(eurostats_data[ISO_COLUMN_NAME]) & set(
-        economist_data[ISO_COLUMN_NAME])
-    if intersection:
-        raise ValueError(
-            'Found duplicated ISOs in Economist and EuroStats excess mortality data: ' + intersection)
+    columns = [ISO_COLUMN_NAME, DATE_COLUMN_NAME, 'deaths_excess_daily_avg', 'deaths_excess_weekly']
 
     excess_mortality = pd.concat([
         economist_data[columns],
-        eurostats_data[columns]
+        eurostats_data[columns],
+        hmd_data[columns]
     ], axis=0)
     return excess_mortality
+
 
 class ExcessMortality():
     """
--- covid19_datasets/hmd.py
+++ covid19_datasets/hmd.py
@@ -12,16 +12,17 @@ _log = logging.getLogger(__name__)
 
 _PATH = 'https://www.mortality.org/Public/STMF/Outputs/stmf.csv'
 COLUMN_NAMES = {
-    'D0_14': 'deaths_excess_age_0_to_14',
-    'D15_64': 'deaths_excess_age_15_to_64',
-    'D65_74': 'deaths_excess_age_65_to_74',
-    'D75_84': 'deaths_excess_age_75_to_84',
-    'D85p': 'deaths_excess_age_85_plus',
-    'DTotal': 'deaths_excess_total'
+    'D0_14': 'deaths_excess_weekly_age_0_to_14',
+    'D15_64': 'deaths_excess_weekly_age_15_to_64',
+    'D65_74': 'deaths_excess_weekly_age_65_to_74',
+    'D75_84': 'deaths_excess_weekly_age_75_to_84',
+    'D85p': 'deaths_excess_weekly_age_85_plus',
+    'DTotal': 'deaths_excess_weekly'
 }
 
 
 def _load_dataset():
+    _log.info(f'Loading data from {_PATH}')
     df = pd.read_csv(_PATH, skiprows=2)
     df = df.drop([
         'R0_14',	
@@ -34,7 +35,18 @@ def _load_dataset():
         'SplitSex',	
         'Forecast'], axis='columns')
     df = df.rename(columns={'CountryCode': ISO_COLUMN_NAME})
+    _log.info('Loaded')
     return df
+
+
+def _resample(grouped_df):
+    # Dates are the end of the week, so we move the min date back to the beginning of that week
+    min_date = grouped_df.DATE.min() - pd.offsets.Day(6)
+    max_date = grouped_df.DATE.max()
+    grouped = grouped_df.set_index('DATE').reindex(
+        pd.date_range(min_date, max_date, freq='D'))
+    grouped.loc[:, 'deaths_excess_daily_avg'] = grouped.loc[:, 'deaths_excess_daily_avg'].bfill(limit=7)
+    return grouped
 
 
 class HMDExcessMortality:
@@ -78,4 +90,16 @@ class HMDExcessMortality:
         excess = (current - baseline).dropna(how='all').reset_index()
         excess[DATE_COLUMN_NAME] = excess['Week'].apply(lambda w: last_day_of_calenderweek(2020, w))
         excess['Sex'] = excess['Sex'].replace({'b': 'Total', 'm': 'Male', 'f': 'Female'})   
-        return excess.rename(columns=COLUMN_NAMES)
+
+        excess = excess.rename(columns=COLUMN_NAMES)
+        excess['deaths_excess_daily_avg'] = excess['deaths_excess_weekly'] / 7.0
+
+        if daily:
+            excess = (excess
+                      .groupby(['ISO', 'Sex'])
+                      .apply(_resample)
+                      .drop(['ISO', 'Sex', 'Week'], axis='columns')
+                      .reset_index()
+                      .rename(columns={'level_2': 'DATE'}))
+
+        return excess
