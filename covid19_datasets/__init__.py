ad4e0aaaae539b96886c5a3a3b5ef6c4e0808f35
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-05-28 16:46:08 +0100

    Remove median ages

--- covid19_datasets/__init__.py
+++ covid19_datasets/__init__.py
@@ -2,7 +2,7 @@ from .oxford_government_policy import OxfordGovernmentPolicyDataset
 from .world_bank import WorldBankDataBank, WORLD_BANK_INDICATORS, AGGREGATES
 from .yougov_behavioural_tracker import YouGovBehaviouralTracker
 from .acaps_government_measures import AcapsGovernmentMeasures
-from .our_world_in_data import OWIDCovid19, OWIDMedianAges
+from .our_world_in_data import OWIDCovid19
 from .uk_area_stats import UKCovid19Data
 from .mask_policies import MaskPolicies
 from .mobility import Mobility
