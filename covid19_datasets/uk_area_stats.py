1e08e1ce2f3f4b07616f74cac3d4ac94d9ac07dc
Avishkar Bhoopchand <avi@avi-net.co.za>
2020-08-03 13:15:31 +0100

    Param to switch between upper tier and lower tier for England

--- covid19_datasets/uk_area_stats.py
+++ covid19_datasets/uk_area_stats.py
@@ -9,6 +9,8 @@ _log = logging.getLogger(__name__)
 
 ENGLAND_CASES_PATH = 'https://c19downloads.azureedge.net/downloads/csv/coronavirus-cases_latest.csv'
 ENGLAND_DEATHS_PATH = 'https://c19downloads.azureedge.net/downloads/csv/coronavirus-deaths_latest.csv'
+ENGLAND_UPPER_TIER_AUTHORITY = 'Upper tier local authority'
+ENGLAND_LOWER_TIER_AUTHORITY = 'Lower tier local authority'
 
 WALES_PATH = 'http://www2.nphs.wales.nhs.uk:8080/CommunitySurveillanceDocs.nsf/61c1e930f9121fd080256f2a004937ed/77fdb9a33544aee88025855100300cab/$FILE/Rapid%20COVID-19%20surveillance%20data.xlsx'
 
@@ -34,7 +36,7 @@ def _backfill_missing_data(df):
     return df
 
 
-def _load_england_cases_dataset():
+def _load_england_cases_dataset(area_type):
     _log.info("Loading dataset from " + ENGLAND_CASES_PATH)
     df = pd.read_csv(ENGLAND_CASES_PATH)
     _log.info("Loaded")
@@ -45,7 +47,7 @@ def _load_england_cases_dataset():
     # Each row corresponds to an area
     # Each column corresponds to a date
     df['Daily lab-confirmed cases'] = df['Daily lab-confirmed cases'].astype('float')
-    df = df[df['Area type'] == 'Upper tier local authority']
+    df = df[df['Area type'] == area_type]
     df['Country'] = 'England'
 
     df = df.pivot_table(index=['Country', 'Area name'], columns=DATE_COLUMN_NAME,
@@ -115,15 +117,16 @@ class UKCovid19Data:
     wales_tests_data = None
     scotland_cases_data = None
 
-    def __init__(self, force_load=False):
+    def __init__(self, force_load=False, england_area_type=ENGLAND_UPPER_TIER_AUTHORITY):
         """
         Loads datasets and store them in memory.
         Further instances of this class will reuse the same data
 
         :param force_load: If true, forces download of the dataset, even if it was loaded already
         """
-        if UKCovid19Data.england_cases_data is None or force_load:
-            UKCovid19Data.england_cases_data = _load_england_cases_dataset()
+        if UKCovid19Data.england_cases_data is None or force_load or UKCovid19Data.england_area_type != england_area_type:
+            UKCovid19Data.england_area_type = england_area_type
+            UKCovid19Data.england_cases_data = _load_england_cases_dataset(england_area_type)
 
         if UKCovid19Data.wales_cases_data is None or UKCovid19Data.wales_tests_data is None or force_load:
             UKCovid19Data.wales_cases_data, UKCovid19Data.wales_tests_data = _load_wales_datasets()
